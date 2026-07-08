2eef5ea833ce66bf2ca057336f682a9e00bed458
Iyyappan <iyyappanv01@gmail.com>
2026-07-08 21:19:43 +0530

    yes

--- controlStatement/Statements.js
+++ controlStatement/Statements.js
@@ -7,13 +7,13 @@ statement};
  */
 
 let age = 18;
-if(age>=18){
-    console.log("you elgible for vote")
+if (age >= 18) {
+  console.log("you elgible for vote");
 }
 
 //if else STATTEMENT____________
 
- /*
+/*
  Syntax:
  if(condition){
  statement
@@ -23,19 +23,16 @@ if(age>=18){
    } 
  */
 
- const uAge  = 20;
- if(uAge>=18){
-    console.log("This Person is Eligible For Voting because, our age was",uAge);
-    
- }
- else{
-    console.log("This Person Not Eligible For Vote!!!");
-     
- }
+const uAge = 20;
+if (uAge >= 18) {
+  console.log("This Person is Eligible For Voting because, our age was", uAge);
+} else {
+  console.log("This Person Not Eligible For Vote!!!");
+}
 
- //___else if ladder __________________
+//___else if ladder __________________
 
- /*
+/*
  Syntax:
  if(condition){
  statement}
@@ -45,41 +42,89 @@ if(age>=18){
     statement} */
 
 const mark = 50;
-if (mark>=90){
-    console.log(`${"This student's Rank was Grade 1 because, the mark is"}`,mark);
-    
-}
-else if(mark>=80){
-    console.log(`${"this student's Rank is Grade 2 mark is"}`,mark);
-    
-}
-else if(mark>=50){
-    console.log(`${"This student had an 3rd Grade because his mark is"}`,mark);
-    
-}
-else{
-    console.log("The student was FAIL");
-    
+if (mark >= 90) {
+  console.log(
+    `${"This student's Rank was Grade 1 because, the mark is"}`,
+    mark,
+  );
+} else if (mark >= 80) {
+  console.log(`${"this student's Rank is Grade 2 mark is"}`, mark);
+} else if (mark >= 50) {
+  console.log(`${"This student had an 3rd Grade because his mark is"}`, mark);
+} else {
+  console.log("The student was FAIL");
 }
 
 //___SWITCH CASE STATEMENT_________________
 
-let  day  = 44;
-switch(day){
-    case 1: console.log("Sunday");
+let day = 4;
+switch (day) {
+  case 1:
+    console.log("Sunday");
     break;
-    case 2 : console.log("Monday");
+  case 2:
+    console.log("Monday");
     break;
-    case 3 : console.log("Tuesday");
+  case 3:
+    console.log("Tuesday");
     break;
-    case 4 : console.log("Wednessday");
+  case 4:
+    console.log("Wednessday");
     break;
-    case 5 : console.log("Thursday");
+  case 5:
+    console.log("Thursday");
     break;
-    case 6 : console.log("Friday");
+  case 6:
+    console.log("Friday");
     break;
-    case 7 : console.log("Saturday");
+  case 7:
+    console.log("Saturday");
     break;
-    default : console.log("Invalid");
-    
+  default:
+    console.log("Invalid");
 }
+
+// Fall through method in Switch Case Statement
+let days = "S1nday";
+switch (days) {
+  case "Monday":
+  case "Tuesday":
+  case "Wednessday":
+  case "Thursday":
+  case "Friday":
+    console.log("WEEK DAY");
+
+    break;
+  case "Saturday":
+  case "Sunday":
+    console.log("WEEKEND");
+    break;
+    default: console.log("it is not a day");
+
+    //Nested if ______________________________________
+
+    let age =2;
+    let height = 175;
+    let weight = 80;
+    if(age>=20){
+        if(height>=170){
+            if(weight<=85){
+                console.log("You sre Selected!!!");
+                
+            }
+            else{
+                console.log("your weight is over weight");
+                
+            }
+        }
+        else{
+            console.log("Your height was low");
+            
+        }
+    }
+     else{
+        console.log("Your age is not compatitable");
+         }
+}
+ 
+     
