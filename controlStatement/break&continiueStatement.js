c66973befcfcb4c4e2b7b781ca862fa8066d2e6d
Iyyappan <iyyappanv01@gmail.com>
2026-07-12 21:15:09 +0530

    new

---
+++ controlStatement/break&continiueStatement.js
@@ -0,0 +1,34 @@
+//Break Statement in js
+
+
+
+for(a=1;a<=20;a++){  
+    if(a%2==0){
+         console.log(a);
+         if(a===10){ break;}   
+    }
+}
+
+//Continue Statement 
+
+for(i=10;i<=100;i++){ 
+if(i===90){continue;}
+console.log(i);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
